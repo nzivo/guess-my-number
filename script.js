5bfb98aeeb0ed71db6e1ace01f957da09c5d6ca9
nzivo <johnnnzivo@gmail.com>
2021-03-15 16:41:02 +0300

    Guess my Number V.1

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -14,6 +15,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(
@@ -41,4 +47,18 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('body').style.backgroundColor = '#ff0000';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.guess').textContent = '';
+
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('body').style.backgroundColor = '#222';
 });
